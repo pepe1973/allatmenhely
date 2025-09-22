1031173470695f748e305b26d704abb04d9c233f
pepe1973 <33117760+pepe1973@users.noreply.github.com>
2025-09-22 13:34:42 +0200

    2025.09.22. Mentés

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,26 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("node:path");
+const adatok_brit = require("./public/js/adatok_brit");
 
 const app = express();
 const PORT = process.env.PORT || 3700;
 
+app.use(cors());
+app.use(express.static(path.resolve(__dirname, "public")));
+
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
 
-app.get('/dogs/csivavas', (req, res) => {
-    try {
-        res.statusCode = 200;
-        return res.json({msg: 'Kutyák/csivavas'});
-    } catch (error) {
-        res.statusCode = 500;
-        return res.json({msg: 'Valami hiba történt!'});
-    }
+app.get("/dogs/csivavas", (req, res) => {
+  console.log(adatok_brit);
+  try {
+    res.statusCode = 200;
+    return res.json({ msg: "Kutyák/csivavas" });
+  } catch (error) {
+    res.statusCode = 500;
+    return res.json({ msg: "Valami hiba történt!" });
+  }
 });
--- frontend/src/components/Csivavas.jsx
+++ frontend/src/components/Csivavas.jsx
@@ -1,24 +1,38 @@
 import { useState, useEffect } from "react";
 import Csivava from "./Csivava";
-import adatok from '../../public/js/adatok';
+import adatok from "../../public/js/adatok";
 
 const Csivavas = () => {
-    let [csivavaItems, setCsivavaItems] = useState([]);
-    let tomb = [];
+  let [csivavaItems, setCsivavaItems] = useState([]);
+  let tomb = [];
 
-    useEffect(() => {
-        for (let i = 0; i < adatok.length; i++) {
-            tomb.push(<Csivava key={i} csiva={adatok[i]} />);
-        }
-    
-        setCsivavaItems(tomb);
-    }, []);
+  useEffect(() => {
+    const leker = async () => {
+      const adatok = await fetch("http://localhost:3700/dogs/csivavas");
 
-    return (
-        <div>
-            <div className="main-kontener">{csivavaItems}</div>;
-        </div>
-    );
-}
+      if (adatok.ok) {
+        const ered = await adatok.json();
+        console.log(ered);
+      } else {
+        const hiba = await adatok.json();
+        console.log(hiba);
+      }
+    };
+
+    leker();
+
+    for (let i = 0; i < adatok.length; i++) {
+      tomb.push(<Csivava key={i} csiva={adatok[i]} />);
+    }
+
+    setCsivavaItems(tomb);
+  }, []);
+
+  return (
+    <div>
+      <div className="main-kontener">{csivavaItems}</div>;
+    </div>
+  );
+};
 
 export default Csivavas;
